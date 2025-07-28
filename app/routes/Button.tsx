68a925d236af4a093fe6546823efc724cef5b04e
ashok.kaushik <ashok.kaushik@zohocorp.com>
2025-07-28 18:23:12 +0530

    Feats:Implements audio slider control for video player and changes setup screen button colors

--- app/routes/Button.tsx
+++ app/routes/Button.tsx
@@ -3,10 +3,11 @@ type buttonProps={
     buttonClass:string,
 };
 export default  function Button({ buttonLabel, buttonClass }:buttonProps){
-    const buttonColor = (buttonClass === 'primary')?'red':'blue';
+    const bgColor:string = (buttonClass === 'primary')?'bg-red-700':'bg-blue-700';
+    const shadowColor:string = (buttonClass === 'primary')?'shadow-red-700':'shadow-blue-700';
     return (
         <>
-            <button className={`bg-${buttonColor}-700 text-white border rounded-lg font-overpass p-2 shadow-${buttonColor}-700 active:transform: translateY(2px);`}>{buttonLabel}</button>
+            <button className={`${bgColor} text-white border rounded-lg font-overpass p-2 ${shadowColor} active:transform: translateY(2px);`}>{buttonLabel}</button>
         </>
     );
 }
