0fceb042cd2249c789e901a6581fede526f51a2d
Ashok Kaushik <kaushikashok45@gmail.com>
2025-12-01 23:03:20 +0530

    Feat : Commits footer file

---
+++ app/routes/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className={`absolute -bottom-40 w-full text-center p-2 bg-gray-50 dark:bg-gray-600`}>
+          <p className={`font-overpass`}>Made with ❤️ by Ashok Kaushik</p>
+        </footer>
+    )
+}
