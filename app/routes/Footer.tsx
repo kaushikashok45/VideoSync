aeef05bd7865d66172cd6d43641dbcdb01d004cc
Ashok Kaushik <kaushikashok45@gmail.com>
2025-12-02 00:18:13 +0530

    Feat : Changes footer position to relative

--- app/routes/Footer.tsx
+++ app/routes/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
-    return (
-        <footer className={`absolute -bottom-40 w-full text-center p-2 bg-gray-50 dark:bg-gray-600`}>
-          <p className={`font-overpass`}>Made with ❤️ by Ashok Kaushik</p>
-        </footer>
-    )
+  return (
+    <footer
+      className={`relative -bottom-40 w-full text-center p-2 bg-gray-50 dark:bg-gray-600`}
+    >
+      <p className={`font-overpass`}>Made with ❤️ by Ashok Kaushik</p>
+    </footer>
+  );
 }
