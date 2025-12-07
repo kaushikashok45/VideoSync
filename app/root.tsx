a5e19a708a5280880a2fbf01c2feca1a130f0248
Ashok Kaushik <kaushikashok45@gmail.com>
2025-12-07 11:56:46 +0530

    Feat : Adds global error boundary for remix app errors

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+    Link,
     Links,
     Meta,
     Outlet,
@@ -15,11 +16,55 @@ import RoomIdContextProvider from "~/routes/RoomIdContextProvider";
 import { APP_NAME } from "~/Constants";
 import Footer from "~/routes/Footer";
 import favicon from "../public/thesyncpartyfavicon.png";
+import errorImg from "../public/errorImg.png";
 
 export const links: LinksFunction = () => [
     { rel: "stylesheet", href: fontsUrl },
     { rel: "stylesheet", href: styles },
 ];
+
+import { isRouteErrorResponse, useRouteError } from "@remix-run/react";
+import UnifiedButton from "./components/UnifiedButton";
+
+export function ErrorBoundary() {
+    const error = useRouteError();
+
+    if (isRouteErrorResponse(error)) {
+        return (
+            <div>
+                <h1>
+                    {error.status} {error.statusText}
+                </h1>
+                <p>{error.data}</p>
+            </div>
+        );
+    } else if (error instanceof Error) {
+        return (
+            <div className="flex flex-col h-dvh w-full justify-center items-center gap-3">
+                <div
+                    id="error-img-wrapper"
+                    className="max-h-[200px] max-w-[200px] rounded-lg"
+                >
+                    <img
+                        src={errorImg}
+                        className="max-h-full max-w-full rounded-lg"
+                    ></img>
+                </div>
+                <h1 className="font-yesteryear text-red-600 text-2xl md:text-4xl">
+                    It isn't you, it's us ! Something went wrong on our end
+                </h1>
+                <p className="font-overpass text-sm px-2">
+                    Click the below button to get back to the home page.
+                </p>
+                <Link to="/">
+                    <UnifiedButton buttonLabel="Home"></UnifiedButton>
+                </Link>
+            </div>
+        );
+    } else {
+        return <h1>Unknown Error</h1>;
+    }
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
@@ -54,6 +99,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
                             theme="system"
                         />
                         <Header />
+
                         <main
                             id="content-container"
                             className="flex-1 w-full flex"
--- app/routes/Header.tsx
+++ app/routes/Header.tsx
@@ -1,12 +1,15 @@
+import { Link } from "@remix-run/react";
 import { APP_NAME } from "~/Constants";
 
 export default function Header() {
     return (
         <header className={`w-full mt-4`}>
             <div id="app-name" className={`text-center`}>
-                <h1 className={`font-yesteryear text-4xl text-red-600`}>
-                    {APP_NAME}
-                </h1>
+                <Link to="/">
+                    <h1 className={`font-yesteryear text-4xl text-red-600`}>
+                        {APP_NAME}
+                    </h1>
+                </Link>
             </div>
         </header>
     );
