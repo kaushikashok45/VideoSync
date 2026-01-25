244aa0c85575d40ac8e97436ee608326fd54dd58
kaushikashok45 <kaushikashok45>
2026-01-25 22:55:04 +0530

    Feat: Adds react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import * as fs from "node:fs";
+import ReactCompilerConfig from "./react-compiler.config.json";
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -14,7 +15,13 @@ declare module "react-router" {
 
 export default defineConfig({
   plugins: [
-    reactRouter(),
+    reactRouter({
+      babel: {
+        plugins: [
+          ['babel-plugin-react-compiler', ReactCompilerConfig],
+        ],
+      },
+    }),
     tsconfigPaths(),
   ],
   server: isDev
